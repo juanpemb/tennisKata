49549cb543ba89dff466020310fe9fa132d5d44d
juanpemb_medianet <juanpedro.martinez@medianet.es>
2015-10-05 11:35:48 +0200

    reset js template and README

--- js/tennisGame.js
+++ js/tennisGame.js
@@ -1,4 +1,4 @@
 function startTheGame(){
-	return "All in love";
 }
+
 
--- js/tests.js
+++ js/tests.js
@@ -1,3 +1,7 @@
 QUnit.test( "add a empty string", function( assert ) {
-  assert.equal(startTheGame(),"All in love","the score should be ´All in love");  
+  assert.equal(startTheGame(),"All in love","the score when staring should be ´All in love");  
+});
+
+QUnit.test( "player 1 score first ball", function( assert ) {
+  assert.equal(playerOneScore(),"fifteen,love","the score when player win the first point is 'fifteen,love'");  
 });
