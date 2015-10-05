bcfa2e8fad016dcd9c9a03e49aeb1312e7dab335
juanpemb_medianet <juanpedro.martinez@medianet.es>
2015-10-05 08:16:40 +0200

    add the js template

---
+++ js/tennisGame.js
@@ -0,0 +1,4 @@
+function startTheGame(){
+	return "All in love";
+}
+
---
+++ js/tests.js
@@ -0,0 +1,3 @@
+QUnit.test( "add a empty string", function( assert ) {
+  assert.equal(startTheGame(),"All in love","the score should be ´All in love");  
+});
